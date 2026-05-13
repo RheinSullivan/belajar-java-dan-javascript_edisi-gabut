e7168d39f7a4ec1c91bf955d4bf409a2925506b8
RheinSullivan <rheinsullivan06@gmail.com>
2026-05-14 00:09:52 +0700

    Hello Java

---
+++ java/JavaPemula/src/main/java/com/priasolo/javapemula/HaloJawa.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.priasolo.javapemula;
+
+/**
+ *
+ * @author rheinsullivan
+ */
+public class HaloJawa { // Wajib huruf kapital disetiap awal kata atau disebut dengan Pascal Case "public class HaloPriaSolo {}", nama variabel/class wajib sama dengan nama file nya "HaloPriaSolo.java".
+    public static void main(String[] args) { //Wajib huruf non kapital atau disebut dengan Camel Case "haloPriaSolo" contoh "public void haloPriaSolo()". "public static void main(String args[] {} adalah pemrosesan program java "main" adalah menthod, bagian ini wajib ada disetiap program java.
+        System.out.println("Halo Jawa"); // println = console.log di JawaSkript, dengan tipe data string ("Halo Pria Solo")
+    }
+}
+
+// Class, variabel, dan method adalah Identifier, diawali dengan (A-Z) atau (a-z). Dapat di isi dengan kombinasi karakter seperti: $ (dollar), atau _ (underscore).
+// Kata kunci Java (reserved word) gk bisa dipake sebagai Identifier, karena Identifier bersifat Case Sensitive.
+
+// Modifier akses seperti default, public, proected, dan private.
+// Modifier non akses seperti final, abstract, dan stricfp.
+// Reserved Word yg gk bisa dipake buat nama konstanta, variabel, atau Identifier:
+//    abstract, byte, class, do, extends, for, import, long, private, short, switch, throws, volatile, assert, case, const, double final, goto, instanceof, nativem proteted, static, while, boolean, catch, if, else, int, public, this, try, defult, eneum, float, interface, return, throw, void, dll.
+
+// Ini adalah tag komentar single-line, komentar ini gk perlu pake penutup "//".
+/* Ini adalah tag komentar multi-line, komentar ini perlu penutup "*" dan "/". */
